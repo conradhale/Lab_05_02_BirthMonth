b45ff52634401d729f697c51712edfad6063c4e8
conradhale <halec5@mail.uc.edu>
2022-09-22 14:19:16 -0400

    Ask for birth month

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    int birthMonth = 23;
+    System.out.println("Enter your birth month: ");
+    int birthMonth = 5;
     if (birthMonth >= 1 && birthMonth <= 12) {
       System.out.println("Your birth month is: " + birthMonth);
     } else {
